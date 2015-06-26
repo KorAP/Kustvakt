cafa03e8d3b04e5a902bfeabb68355d6f5fe61a6
Michael Hanl <hanl@ids-mannheim.de>
2015-06-26 17:01:16 +0200

    groupid koral query

--- src/main/java/de/ids_mannheim/korap/config/QueryBuilderUtil.java
+++ src/main/java/de/ids_mannheim/korap/config/QueryBuilderUtil.java
@@ -1,8 +1,22 @@
 package de.ids_mannheim.korap.config;
 
+import de.ids_mannheim.korap.query.serialize.MetaQueryBuilder;
+
 /**
  * @author hanl
  * @date 25/06/2015
  */
 public class QueryBuilderUtil {
+
+    public static MetaQueryBuilder defaultMetaBuilder(Integer pageIndex,
+            Integer pageInteger, Integer pageLength, String ctx,
+            Boolean cutoff) {
+        MetaQueryBuilder meta = new MetaQueryBuilder();
+        meta.addEntry("startIndex", pageIndex)
+                .addEntry("startPage", pageInteger)
+                .addEntry("count", pageLength).setSpanContext(ctx)
+                .addEntry("cutOff", cutoff);
+        return meta;
+
+    }
 }
