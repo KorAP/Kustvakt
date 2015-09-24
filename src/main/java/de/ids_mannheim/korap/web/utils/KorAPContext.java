e3fe5cd058b1b50250d9dd4c5a803e4d2efc7ac4
Michael Hanl <hanl@ids-mannheim.de>
2015-09-24 19:43:57 +0200

    authentication context holder

---
+++ src/main/java/de/ids_mannheim/korap/web/utils/KorAPContext.java
@@ -0,0 +1,42 @@
+package de.ids_mannheim.korap.web.utils;
+
+import de.ids_mannheim.korap.user.TokenContext;
+
+import javax.ws.rs.core.SecurityContext;
+import java.security.Principal;
+
+/**
+ * @author hanl
+ * @date 13/05/2014
+ *
+ * wrapper for REST security context
+ *
+ */
+public class KorAPContext implements SecurityContext {
+
+    private TokenContext user;
+
+    public KorAPContext(final TokenContext user) {
+        this.user = user;
+    }
+
+    @Override
+    public Principal getUserPrincipal() {
+        return this.user;
+    }
+
+    @Override
+    public boolean isUserInRole(String role) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean isSecure() {
+        return false;
+    }
+
+    @Override
+    public String getAuthenticationScheme() {
+        return SecurityContext.BASIC_AUTH;
+    }
+}
