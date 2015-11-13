48700a734fad2b015127fc46e8b7a59ef7dda86c
Michael Hanl <hanl@ids-mannheim.de>
2015-11-13 16:18:50 +0100

    rewrite post processor; refactoring; tests

---
+++ src/main/java/de/ids_mannheim/korap/resource/rewrite/DocMatchRewrite.java
@@ -0,0 +1,22 @@
+package de.ids_mannheim.korap.resource.rewrite;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+/**
+ * @author hanl
+ * @date 12/11/2015
+ */
+public class DocMatchRewrite implements RewriteTask.RewriteAfter {
+
+
+
+    @Override
+    public JsonNode postProcess(KoralNode node) {
+
+
+
+
+
+        return null;
+    }
+}
