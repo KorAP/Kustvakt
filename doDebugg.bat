dd8024260b94cb74df9409fb98742743e83068d9
margaretha <margaretha@ids-mannheim.de>
2017-05-05 13:57:25 +0200

    Removed auto generated eclipse related files.
    Change-Id: I3174d867d41c4781bc2696ddb742bee571953d63

--- doDebugg.bat
+++
@@ -1,9 +0,0 @@
-: skript zum Remote Debuggen von eclipse's Debugger aus.
-
-set OPTS=-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005
-set JAR=-jar target/Kustvakt-core-0.59.7.jar
-
-java %OPTS% %JAR%
-
-
-
