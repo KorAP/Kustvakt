698d95307b65ae9f3607d35774330e5f2d0fc8a9
margaretha <margaretha@ids-mannheim.de>
2017-06-27 10:53:27 +0200

    Disabled license checking in light (matchinfo) service.
    Change-Id: I6a11a0073ad173ce6cf0fe77107f78b0c8816816

--- src/main/java/de/ids_mannheim/korap/resource/rewrite/CollectionRewrite.java
+++ src/main/java/de/ids_mannheim/korap/resource/rewrite/CollectionRewrite.java
@@ -48,7 +48,8 @@ public class CollectionRewrite implements RewriteTask.RewriteQuery {
                 && node.at("/key").asText().equals("availability")) {
             String queryAvailability = node.at("/value").asText();
             String matchOp = node.at("/match").asText();
-            if (!userAvailabilities.contains(queryAvailability)){
+            if (userAvailabilities.contains(queryAvailability) && matchOp.
+                    equals(KoralMatchOperator.EQUALS.toString())){
                 userAvailabilities.remove(queryAvailability);
             }
         }
