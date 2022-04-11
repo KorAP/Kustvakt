d7163120e8d25ff4c72bc8e6646e8e23e8596c00
margaretha <margaretha@ids-mannheim.de>
2022-04-11 09:42:41 +0200

    Added columns to the oauth2_client table and updated client registration
    Change-Id: Ic16b41859fdda464fc2315ae40b1ddded9c45dca

---
+++ full/src/main/resources/db/sqlite/V1.11__plugin.sql
@@ -0,0 +1,22 @@
+ALTER TABLE oauth2_client 
+	ADD COLUMN registration_date TIMESTAMP NOT NULL;
+
+-- default 365 days in seconds
+ALTER TABLE oauth2_client 
+	ADD COLUMN refresh_token_expiry INTEGER DEFAULT 31536000;
+	
+ALTER TABLE oauth2_client 
+	ADD COLUMN source BLOB DEFAULT NULL;
+
+ALTER TABLE oauth2_client 
+	ADD COLUMN is_permitted BOOLEAN DEFAULT FALSE;
+
+--CREATE TABLE IF NOT EXISTS user_installed_client (
+--	id INTEGER PRIMARY KEY AUTOINCREMENT,
+--	installed_by VARCHAR(100) NOT NULL,
+--	installed_date TIMESTAMP NOT NULL,
+--	client_id VARCHAR(100) NOT NULL,
+--	FOREIGN KEY (client_id)
+--	   REFERENCES oauth2_client(id)
+--	   ON DELETE CASCADE
+--);
