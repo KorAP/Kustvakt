57b8a7483eb0b6133069ba5dd256e0542e7b3c82
Marc Kupietz <kupietz@ids-mannheim.de>
2022-04-30 18:37:03 +0200

    Automatically start embedded LDAP server if configured
    Automatically start on first login attempt, if
    useEmbeddedServer=true in ldap.conf
    
    Change-Id: Id81a4d23a4e205d05545b22a5655ebd5eb25599b

---
+++ full/src/test/java/de/ids_mannheim/korap/server/EmbeddedLdapServerTest.java
@@ -0,0 +1,43 @@
+package de.ids_mannheim.korap.server;
+
+import com.unboundid.ldap.sdk.LDAPException;
+import com.unboundid.util.Base64;
+import com.unboundid.util.StaticUtils;
+import de.ids_mannheim.korap.authentication.LdapAuth3;
+import org.junit.AfterClass;
+import org.junit.Test;
+
+import static de.ids_mannheim.korap.authentication.LdapAuth3.LDAP_AUTH_ROK;
+import static de.ids_mannheim.korap.authentication.LdapAuth3.LDAP_AUTH_RUNKNOWN;
+import static org.junit.Assert.assertEquals;
+
+public class EmbeddedLdapServerTest {
+
+    @AfterClass
+    public static void shutdownEmbeddedLdapServer() {
+        EmbeddedLdapServer.stop();
+    }
+
+    @Test
+    public void embeddedServerStartsAutomaticallyAndUsersCanLogin() throws LDAPException {
+        final byte[] passwordBytes = StaticUtils.getBytes("password");
+        String pw = Base64.encode(passwordBytes);
+
+        assertEquals(LDAP_AUTH_ROK, LdapAuth3.login("user", pw, "src/main/resources/ldap.properties"));
+    }
+
+    @Test
+    public void usersWithUnencodedPasswowrdCanLogin() throws LDAPException {
+        assertEquals(LDAP_AUTH_ROK, LdapAuth3.login("user1", "password1", "src/main/resources/ldap.properties"));
+    }
+
+    @Test
+    public void asteriskPasswordsFail() throws LDAPException {
+        assertEquals(LDAP_AUTH_RUNKNOWN, LdapAuth3.login("user1", "*", "src/main/resources/ldap.properties"));
+    }
+
+    @Test
+    public void unauthorizedUsersAreNotAllowed() throws LDAPException {
+        assertEquals(LDAP_AUTH_RUNKNOWN, LdapAuth3.login("yuser", "password", "src/main/resources/ldap.properties"));
+    }
+}
