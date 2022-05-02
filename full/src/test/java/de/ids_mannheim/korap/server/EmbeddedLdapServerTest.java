43c81b361e6ad279f10db845913e760c3d7c77f6
Marc Kupietz <kupietz@ids-mannheim.de>
2022-05-02 13:23:18 +0200

    Rename ldap .properties to .conf
    Change-Id: I8a6a49fa5c8ae1e03c9f699d22825a767b8041ea

--- full/src/test/java/de/ids_mannheim/korap/server/EmbeddedLdapServerTest.java
+++ full/src/test/java/de/ids_mannheim/korap/server/EmbeddedLdapServerTest.java
@@ -13,6 +13,8 @@ import static org.junit.Assert.assertEquals;
 
 public class EmbeddedLdapServerTest {
 
+    public static final String EMBEDDED_LDAP_DEFAULT_CONF = "src/main/resources/embedded-ldap-default.conf";
+
     @AfterClass
     public static void shutdownEmbeddedLdapServer() {
         EmbeddedLdapServer.stop();
@@ -23,21 +25,21 @@ public class EmbeddedLdapServerTest {
         final byte[] passwordBytes = StaticUtils.getBytes("password");
         String pw = Base64.encode(passwordBytes);
 
-        assertEquals(LDAP_AUTH_ROK, LdapAuth3.login("user", pw, "src/main/resources/ldap.properties"));
+        assertEquals(LDAP_AUTH_ROK, LdapAuth3.login("user", pw, EMBEDDED_LDAP_DEFAULT_CONF));
     }
 
     @Test
     public void usersWithUnencodedPasswowrdCanLogin() throws LDAPException {
-        assertEquals(LDAP_AUTH_ROK, LdapAuth3.login("user1", "password1", "src/main/resources/ldap.properties"));
+        assertEquals(LDAP_AUTH_ROK, LdapAuth3.login("user1", "password1", EMBEDDED_LDAP_DEFAULT_CONF));
     }
 
     @Test
     public void asteriskPasswordsFail() throws LDAPException {
-        assertEquals(LDAP_AUTH_RUNKNOWN, LdapAuth3.login("user1", "*", "src/main/resources/ldap.properties"));
+        assertEquals(LDAP_AUTH_RUNKNOWN, LdapAuth3.login("user1", "*", EMBEDDED_LDAP_DEFAULT_CONF));
     }
 
     @Test
     public void unauthorizedUsersAreNotAllowed() throws LDAPException {
-        assertEquals(LDAP_AUTH_RUNKNOWN, LdapAuth3.login("yuser", "password", "src/main/resources/ldap.properties"));
+        assertEquals(LDAP_AUTH_RUNKNOWN, LdapAuth3.login("yuser", "password", EMBEDDED_LDAP_DEFAULT_CONF));
     }
 }
