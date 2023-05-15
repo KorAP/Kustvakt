4bc08be5aee3d61cb3a2cac9b7d732fc092de52d
margaretha <margaretha@ids-mannheim.de>
2023-05-15 14:10:00 +0200

    Merged lite into full
    Change-Id: I8e496cebcfa91de5077bc938ec00617136037e22

--- full/src/main/java/de/ids_mannheim/korap/oauth2/service/DummyOAuth2ScopeServiceImpl.java
+++ full/src/main/java/de/ids_mannheim/korap/oauth2/service/DummyOAuth2ScopeServiceImpl.java
@@ -2,6 +2,7 @@ package de.ids_mannheim.korap.oauth2.service;
 
 import de.ids_mannheim.korap.constant.OAuth2Scope;
 import de.ids_mannheim.korap.exceptions.KustvaktException;
+import de.ids_mannheim.korap.oauth2.service.OAuth2ScopeService;
 import de.ids_mannheim.korap.security.context.TokenContext;
 
 public class DummyOAuth2ScopeServiceImpl implements OAuth2ScopeService {
