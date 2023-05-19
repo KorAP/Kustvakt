949ee382e6a6548b977ef2a146733d58817d302a
margaretha <margaretha@ids-mannheim.de>
2023-05-19 14:08:22 +0200

    Removed old tests.
    Change-Id: Ia4d709e8663e9c113ca02cf8c8260e85d58bac7d

--- full/src/test/java/de/ids_mannheim/korap/web/controller/QuerySerializationControllerTest.java
+++ full/src/test/java/de/ids_mannheim/korap/web/controller/QuerySerializationControllerTest.java
@@ -13,24 +13,24 @@ import static org.junit.Assert.assertTrue;
 import java.util.Iterator;
 
 import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
 import org.junit.Ignore;
 import org.junit.Test;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import javax.ws.rs.core.Response;
 
 import de.ids_mannheim.korap.authentication.http.HttpAuthorizationHandler;
 import de.ids_mannheim.korap.config.Attributes;
+import de.ids_mannheim.korap.config.SpringJerseyTest;
 import de.ids_mannheim.korap.exceptions.KustvaktException;
 import de.ids_mannheim.korap.utils.JsonUtils;
-import de.ids_mannheim.korap.web.FastJerseyTest;
 
 
 // EM: The API is disabled
 @Ignore
-public class QuerySerializationControllerTest extends FastJerseyTest {
+public class QuerySerializationControllerTest extends SpringJerseyTest {
 
     @Test
     public void testQuerySerializationFilteredPublic ()
