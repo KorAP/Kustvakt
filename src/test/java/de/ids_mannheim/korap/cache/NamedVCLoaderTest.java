4478956cd4602bface08cdf96f2fbc519d7c0902
margaretha <margaretha@ids-mannheim.de>
2024-04-10 13:10:02 +0200

    Added KoralQuery check when updating VC (solved #676)
    Change-Id: Ic7f8f565fde6c4a5bf087e27af3f47674854a0f6

--- src/test/java/de/ids_mannheim/korap/cache/NamedVCLoaderTest.java
+++ src/test/java/de/ids_mannheim/korap/cache/NamedVCLoaderTest.java
@@ -34,10 +34,40 @@ public class NamedVCLoaderTest extends SpringJerseyTest {
         assertTrue(VirtualCorpusCache.contains(vcId));
         Map<String, DocBits> cachedData = VirtualCorpusCache.retrieve(vcId);
         assertTrue(cachedData.size() > 0);
-        VirtualCorpusCache.delete(vcId);
-        assertFalse(VirtualCorpusCache.contains(vcId));
+        //VirtualCorpusCache.delete(vcId);
+        //assertFalse(VirtualCorpusCache.contains(vcId));
         QueryDO vc = dao.retrieveQueryByName(vcId, "system");
         assertNotNull(vc);
+        
+        String koralQuery = vc.getKoralQuery();
+        testUpdateVC(vcId,koralQuery);
+    }
+    
+    private void testUpdateVC (String vcId, String koralQuery)
+            throws IOException, QueryException, KustvaktException {
+        String json = """
+            {"collection": {
+                "@type": "koral:doc",
+                "key": "textSigle",
+                "match": "match:eq",
+                "type" : "type:string",
+                "value": [
+                    "GOE/AGF/00000"
+                ]
+            }}""";
+       
+        vcLoader.loadVCToCache(vcId, "", json);
+        
+        Map<String, DocBits> cachedData = VirtualCorpusCache.retrieve(vcId);
+        assertTrue(cachedData.size() > 0);
+        
+        QueryDO vc = dao.retrieveQueryByName(vcId, "system");
+        String updatedKoralQuery = vc.getKoralQuery();
+        
+        assertTrue (koralQuery.hashCode() != updatedKoralQuery.hashCode());
+        
+        VirtualCorpusCache.delete(vcId);
+        assertFalse(VirtualCorpusCache.contains(vcId));
         dao.deleteQuery(vc);
         vc = dao.retrieveQueryByName(vcId, "system");
         assertNull(vc);
