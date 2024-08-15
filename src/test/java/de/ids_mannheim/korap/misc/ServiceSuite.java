e3762da7cef0b68f61fd2b4e36e82a08b438ac4c
margaretha <margaretha@ids-mannheim.de>
2024-08-15 11:35:36 +0200

    Removed empty tests.
    Change-Id: I719dab4de345c0d488ee40cca3d588d333a8fb92

--- src/test/java/de/ids_mannheim/korap/misc/ServiceSuite.java
+++
@@ -1,7 +0,0 @@
-package de.ids_mannheim.korap.misc;
-
-/**
- * @author hanl
- * @date 29/02/2016
- */
-public class ServiceSuite {}
--- src/test/java/de/ids_mannheim/korap/misc/TestNullableSqls.java
+++
@@ -1,7 +0,0 @@
-package de.ids_mannheim.korap.misc;
-
-/**
- * @author hanl
- * @date 30/01/2016
- */
-public class TestNullableSqls {}
